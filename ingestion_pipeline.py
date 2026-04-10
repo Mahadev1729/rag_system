a9e5fbfaf6f97bb854af46971ecf8098b86e5c27
Mahadev1729 <devcampus17@gmail.com>
2026-04-10 19:15:03 +0530

    rag system with ingestion pipeline

---
+++ ingestion_pipeline.py
@@ -0,0 +1,47 @@
+﻿import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+# from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_documents(docs_path='docs'):
+    print(f"loading documents from {docs_path}...")
+    
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exist. please create it ")
+    loader=DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"}  
+    ) 
+    
+    
+    documents=loader.load()
+    
+    if len(documents)==0:
+        raise FileNotFoundError(f"No, txt files found in {docs_path}.please add your company documents")
+    
+    for i, doc in enumerate(documents[:2]):
+        print(f"\n Documents {i+1}")
+        print(f"Source:{doc.metadata['source']}")
+        print(f"Content length:{len(doc.page_content)} characters")
+        print(f"Content preview:{doc.page_content[:100]}...")
+        print(f"metadata:{doc.metadata}")
+    return documents   
+
+load_documents(docs_path="docs")   
+# def main():
+    
+#     # loading the files
+    
+#     # chunking the files
+#     # Embedding and storing in vector DB
+    
+ 
+    
+    
+
